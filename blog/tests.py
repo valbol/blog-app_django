325a516e3e00ca369ec7eaa67f01593b0f6c8c52
Valera B <bvalery87@gmail.com>
2020-06-27 16:39:22 +0300

    forms and usee accounts

--- blog/tests.py
+++ blog/tests.py
@@ -1,23 +1,24 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase, Client
-from django.urls import reverse 
+from django.urls import reverse
 
 from .models import Post
 
+
 class BlogTest(TestCase):
-    
+
     def setUp(self):
         self.user = get_user_model().objects.create_user(
-            username ='testuser',
-            email = 'test@mail.com',
-            password = 'secret'
+            username='testuser',
+            email='test@mail.com',
+            password='secret'
         )
         self.post = Post.objects.create(
-            title = 'A good title',
-            body = 'Nice body content',
-            author = self.user
+            title='A good title',
+            body='Nice body content',
+            author=self.user
         )
-    
+
     def test_string_representation(self):
         post = Post(title='A sample title')
         self.assertEqual(str(post), post.title)
@@ -40,3 +41,25 @@ class BlogTest(TestCase):
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, 'A good title')
         self.assertTemplateUsed(response, 'post_detail.html')
+
+    def test_post_create_view(self):
+        response = self.client.post(reverse('post_new'), {
+            'title': 'New title',
+            'body': 'New text',
+            'author': self.user,
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'New title')
+        self.assertContains(response, 'New text')
+
+    def test_post_update_view(self):
+        response = self.client.post(reverse('post_edit', args='1'), {
+            'title': 'Updated title',
+            'body': 'Updated text',
+        })
+        self.assertEqual(response.status_code, 302)
+
+    def test_post_delete_view(self):
+        response = self.client.get(
+            reverse('post_delete', args='1'))
+        self.assertEqual(response.status_code, 200)
